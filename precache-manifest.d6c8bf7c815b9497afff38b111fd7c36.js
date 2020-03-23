290ff65d79df803e54c26485e02571c46523ddfe
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-03-23 18:54:25 +0530

    Updates

---
+++ precache-manifest.d6c8bf7c815b9497afff38b111fd7c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a05d040a5bb7d2de0b48be27cd5cee2",
+    "url": "/Book-Counter-App/index.html"
+  },
+  {
+    "revision": "e81f8b872757ab67214a",
+    "url": "/Book-Counter-App/static/css/main.54d8cda7.chunk.css"
+  },
+  {
+    "revision": "b601fece8444abb6ae66",
+    "url": "/Book-Counter-App/static/js/2.fd9dc2eb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Book-Counter-App/static/js/2.fd9dc2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e81f8b872757ab67214a",
+    "url": "/Book-Counter-App/static/js/main.cf2c7abf.chunk.js"
+  },
+  {
+    "revision": "c0e586442242927d49f3",
+    "url": "/Book-Counter-App/static/js/runtime-main.e1f6787d.js"
+  }
+]);
